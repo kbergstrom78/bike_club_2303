fc54aeffcfc986f24e662326772d09af04ffb277
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-04-25 10:27:31 -0600

    Complete iteration 2 testing

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -80,6 +80,16 @@ RSpec.describe Biker do
       }
       expect(@biker2.rides).to eq(expected)
     end
+  
+    it 'reports biker2 prs and returns false if not completed' do
+      @biker2.log_ride(@ride1, 97.0)
+      @biker2.log_ride(@ride2, 67.0)
+      @biker2.learn_terrain!(:gravel)
+      @biker2.learn_terrain!(:hills)
+      @biker2.log_ride(@ride1, 95.0)
+      @biker2.log_ride(@ride2, 65.0)
+      expect(@biker2.personal_record(@ride2)).to eq(65.0)
+      expect(@biker2.personal_record(@ride1)).to eq(false)
+    end
   end
-
 end
