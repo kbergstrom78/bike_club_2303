c3625b76972c558d80a7de8256e245ad3875712d
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-04-25 09:08:44 -0600

    #initialize

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,15 @@
 class Ride
+  attr_reader :name,
+              :distance,
+              :terrain,
+              :loop
+
+  def initialize(info)
+    @name = info[:name]
+    @distance = info[:distance]
+    @terrain = info[:terrain]
+    @loop = info[:loop]
+
+  end
 
 end
