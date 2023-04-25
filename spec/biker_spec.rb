892d185acc2b2a8276175a4cd39cc46d41a36930
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-04-25 09:26:09 -0600

    Initialize biker

---
+++ spec/biker_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+require './lib/ride'
+require './lib/biker'
+
+RSpec.describe Biker do
+  before(:each) do
+    @biker = Biker.new("Kenny", 30)
+    @biker2 = Biker.new("Athena", 15)
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+  end
+
+  describe '#initialize' do  
+    it 'exists' do
+      expect(@biker).to be_an_instance_of(Biker)
+    end
+  end
+end
