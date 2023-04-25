1be6dcddff2fccc3a437212af208690c57f4a2ed
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-04-25 10:41:05 -0600

    Initialize BikeClub

---
+++ spec/bike_club.rb
@@ -0,0 +1,19 @@
+require './lib/ride'
+require './lib/biker'
+require './lib/bike_club'
+
+RSpec.describe BikeClub do
+  before(:each) do
+    @bike_club = BikeClub.new('Kicking Asphalt')
+    @biker = Biker.new("Kenny", 30)
+    @biker2 = Biker.new("Athena", 15)
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@bike_club).to be_an_instance_of(BikeClub)
+    end
+  end
+end
